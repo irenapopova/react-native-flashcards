6755b4736b1835d1c87407d82951e03c0ca979d4
Irena <irenejpopova6@gmail.com>
2019-11-26 20:43:39 +0100

    added button

--- components/AddCard.js
+++ components/AddCard.js
@@ -5,6 +5,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class AddCards extends React.Component{
     render(){
-        return <View><Text>AddCard</Text></View>
+        return <View style={styles.card}><Text>AddCard</Text></View>
+
     }
 }
+
+
+const styles = StyleSheet.create({
+    card: {
+      padding: 20,
+      borderColor: 'red',
+      borderWidth: 4,
+      height: 10,
+      color: 'green',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 20,
+    },
+  });
