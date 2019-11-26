dcf26feeda88389638cf2b7ae46cb75430e24abc
Irena <irenejpopova6@gmail.com>
2019-11-26 19:57:32 +0100

    component AddCard added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddCard from './components/AddCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Merry Christmas @everyone!</Text>
+      <AddCard></AddCard>
     </View>
   );
 }
